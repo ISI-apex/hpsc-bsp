5dfc8ddc4d01b55b5233ce67f4c138d62bcacfbd
Connor Imes <cimes@isi.edu>
2018-12-08 11:01:11 -0500

    build-hpsc-baremetal: support online fetch and offline build

--- build-hpsc-baremetal.sh
+++ build-hpsc-baremetal.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Build sources that require the baremetal toolchain
+# Fetch and/or build sources that require the baremetal toolchain
 #
 
 # The following toolchain path may need to be updated:
 GCC_ARM_NONE_EABI_BINDIR=${PWD}/../gcc-arm-none-eabi-7-2018-q2-update/bin
 
-BAREMETAL_BRANCH_TAG="hpsc"
-UBOOT_R52_BRANCH_TAG="hpsc"
+BM_GIT_CHECKOUT="hpsc"
+UBOOT_R52_GIT_CHECKOUT="hpsc"
 
 BUILD_JOBS=4
 
@@ -17,26 +17,49 @@ function check_toolchain()
     which arm-none-eabi-gcc > /dev/null 2>&1
 }
 
-# Checkout repository if not already done, always pull branch/tag
-# $1 = repository name, $2 = branch/tag name, $3 = repository directory
+# Clone repository if not already done, always pull
 function isi_github_pull()
 {
     local repo=$1
-    local brtag=$2
-    local dir=$3
-    echo "Pulling repository=$repo, branch/tag=$brtag"
+    local dir=$2
+    # Don't ask for credentials if requests are bad
+    export GIT_TERMINAL_PROMPT=0 # for git > 2.3
+    export GIT_ASKPASS=/bin/echo
+    echo "Pulling repository=$repo"
     if [ ! -d "$dir" ]; then
-        git clone "https://github.com/ISI-apex/$repo.git" "$dir"
+        git clone "https://github.com/ISI-apex/$repo.git" "$dir" || return $?
     fi
     cd "$dir"
-    # `git pull origin "$brtag"` is causing conflicts...?
-    git checkout "$brtag"
     git pull
-    cd ..
+    RC=$?
+    cd - > /dev/null
+    return $RC
 }
 
-if ! check_toolchain; then
-    PATH=${GCC_ARM_NONE_EABI_BINDIR}:${PATH}
+# Script options
+IS_ONLINE=1
+IS_BUILD=1
+case "$1" in
+    "" | "all")
+        ;;
+    "fetchall")
+        IS_BUILD=0
+        ;;
+    "buildall")
+        IS_ONLINE=0
+        ;;
+    *)
+        echo "Usage: $0 [ACTION]"
+        echo "  where ACTION is one of:"
+        echo "    all: (default) download sources and compile"
+        echo "    fetchall: download sources"
+        echo "    buildall: compile pre-downloaded sources"
+        exit 1
+        ;;
+esac
+
+if [ $IS_BUILD -ne 0 ]; then
+    PATH=${PATH}:${GCC_ARM_NONE_EABI_BINDIR}
     if ! check_toolchain; then
         echo "Error: update GCC_ARM_NONE_EABI_BINDIR or add toolchain bin directory to PATH"
         exit 1
@@ -44,34 +67,57 @@ if ! check_toolchain; then
 fi
 
 ## hpsc-baremetal
-isi_github_pull hpsc-baremetal "$BAREMETAL_BRANCH_TAG" hpsc-baremetal
-cd hpsc-baremetal
-echo "hpsc-baremetal: clean..."
-make clean
-echo "hpsc-baremetal: build..."
-make all -j${BUILD_JOBS}
-RC=$?
-if [ $RC -eq 0 ]; then
-    echo "hpsc-baremetal: build successful"
-else
-    echo "hpsc-baremetal: Error: build failed with exit code: $RC"
-    exit $RC
+DIR="hpsc-baremetal"
+if [ $IS_ONLINE -ne 0 ]; then
+    isi_github_pull hpsc-baremetal "$DIR" || exit $?
 fi
-cd ..
+if [ $IS_BUILD -ne 0 ]; then
+    if [ ! -d "$DIR" ]; then
+        echo "$DIR: Error: must fetch sources before build"
+        exit 1
+    fi
+
+    cd "$DIR" || exit $?
+    echo "$DIR: checkout $BM_GIT_CHECKOUT..."
+    git checkout "$BM_GIT_CHECKOUT" || exit $?
+    echo "$DIR: clean..."
+    make clean
+    echo "$DIR: build..."
+    make all -j${BUILD_JOBS}
+    RC=$?
+    if [ $RC -eq 0 ]; then
+        echo "$DIR: build successful"
+    else
+        echo "$DIR: Error: build failed with exit code: $RC"
+        exit $RC
+    fi
+    cd - > /dev/null
+fi
 
 ## u-boot-r52
-isi_github_pull u-boot "$UBOOT_R52_BRANCH_TAG" u-boot-r52
-cd u-boot-r52
-echo "u-boot-r52: clean..."
-make clean
-echo "u-boot-r52: build..."
-make hpsc_rtps_r52_defconfig
-make -j${BUILD_JOBS} CROSS_COMPILE=arm-none-eabi- CONFIG_LD_GCC=y
-RC=$?
-if [ $RC -eq 0 ]; then
-    echo "u-boot-r52: build successful"
-else
-    echo "u-boot-r52: Error: build failed with exit code: $RC"
-    exit $RC
+DIR="u-boot-r52"
+if [ $IS_ONLINE -ne 0 ]; then
+    isi_github_pull u-boot "$DIR" || exit $?
 fi
-cd ..
+if [ $IS_BUILD -ne 0 ]; then
+    if [ ! -d "$DIR" ]; then
+        echo "$DIR: Error: must fetch sources before build"
+        exit 1
+    fi
+    cd "$DIR" || exit $?
+    echo "$DIR: checkout $UBOOT_R52_GIT_CHECKOUT..."
+    git checkout "$UBOOT_R52_GIT_CHECKOUT" || exit $?
+    echo "$DIR: clean..."
+    make clean
+    echo "$DIR: build..."
+    make hpsc_rtps_r52_defconfig
+    make -j${BUILD_JOBS} CROSS_COMPILE=arm-none-eabi- CONFIG_LD_GCC=y
+    RC=$?
+    if [ $RC -eq 0 ]; then
+        echo "$DIR: build successful"
+    else
+        echo "$DIR: Error: build failed with exit code: $RC"
+        exit $RC
+    fi
+    cd - > /dev/null
+fi
