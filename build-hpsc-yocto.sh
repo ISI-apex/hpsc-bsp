e146d026c28d4d28f933a6046c29a07d7a1a3698
Connor Imes <cimes@isi.edu>
2018-12-16 11:24:27 -0500

    build-hpsc-yocto: do checkouts in fetch step

--- build-hpsc-yocto.sh
+++ build-hpsc-yocto.sh
@@ -42,7 +42,7 @@ function usage()
 
 # Script options
 IS_ONLINE=1
-ACTION=""
+ACTION="all"
 BUILD=""
 WORKING_DIR=""
 # parse options
@@ -79,55 +79,54 @@ while getopts "h?a:b:w:" o; do
     esac
 done
 shift $((OPTIND-1))
-
 if [ -z "$BUILD" ]; then
     usage
 fi
 if [ -z "$WORKING_DIR" ]; then
     WORKING_DIR="$BUILD"
 fi
-. ./build-common.sh || exit $?
+
+# Fail-fast
+set -e
+
+. ./build-common.sh
 build_set_environment "$BUILD"
 
 TOPDIR=${PWD}
-mkdir -p "$WORKING_DIR" || exit 1
+mkdir -p "$WORKING_DIR"
 cd "$WORKING_DIR"
 
-# BB_ENV_EXTRAWHITE allows additional variables to pass through from
-# the external environment into Bitbake's datastore
-export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE \
-                          SRCREV_atf \
-                          SRCREV_linux_hpsc \
-                          SRCREV_qemu_devicetrees \
-                          SRCREV_qemu \
-                          SRCREV_u_boot"
-
 if [ $IS_ONLINE -ne 0 ]; then
-    git_clone_pull "https://github.com/ISI-apex/meta-openembedded" "meta-openembedded" || exit $?
-    git_clone_pull "https://github.com/ISI-apex/meta-hpsc" "meta-hpsc" || exit $?
-    git_clone_pull "https://github.com/ISI-apex/poky" "poky" || exit $?
+    # add the meta-openembedded layer (for the mpich package)
+    git_clone_pull "https://github.com/ISI-apex/meta-openembedded" "meta-openembedded"
+    (
+        cd meta-openembedded
+        assert_str "$GIT_CHECKOUT_META_OE"
+        git checkout "$GIT_CHECKOUT_META_OE"
+    )
+
+    # add the meta-hpsc layer
+    git_clone_pull "https://github.com/ISI-apex/meta-hpsc" "meta-hpsc"
+    (
+        cd meta-hpsc
+        assert_str "$GIT_CHECKOUT_META_HPSC"
+        git checkout "$GIT_CHECKOUT_META_HPSC"
+    )
+
+    # download the yocto poky git repository
+    git_clone_pull "https://github.com/ISI-apex/poky" "poky"
+    (
+        cd poky
+        assert_str "$GIT_CHECKOUT_POKY"
+        git checkout "$GIT_CHECKOUT_POKY"
+    )
 fi
-
-# add the meta-openembedded layer (for the mpich package)
-cd meta-openembedded
-assert_str "$GIT_CHECKOUT_META_OE"
-git checkout "$GIT_CHECKOUT_META_OE" || exit $?
-cd ..
-
-# add the meta-hpsc layer
-cd meta-hpsc
-assert_str "$GIT_CHECKOUT_META_HPSC"
-git checkout "$GIT_CHECKOUT_META_HPSC" || exit $?
-cd ..
-
 BITBAKE_LAYERS=("${PWD}/meta-openembedded/meta-oe"
                 "${PWD}/meta-openembedded/meta-python"
                 "${PWD}/meta-hpsc/meta-xilinx-bsp")
 
 # download the yocto poky git repository
 cd poky
-assert_str "$GIT_CHECKOUT_POKY"
-git checkout "$GIT_CHECKOUT_POKY" || exit $?
 # create build directory and configure it
 . ./oe-init-build-env build
 for layer in "${BITBAKE_LAYERS[@]}"; do
@@ -140,21 +139,17 @@ conf_replace_or_append "CORE_IMAGE_EXTRA_INSTALL" "\"${YOCTO_INSTALL[*]}\""
 conf_replace_or_append "FORTRAN_forcevariable" "\",fortran\""
 
 # finally, execute the requested action
-case "$ACTION" in
-    "" | "all" | "buildall")
-        bitbake core-image-minimal
-        ;;
-    "fetchall")
-        bitbake core-image-minimal -c fetchall
-        ;;
-    "populate_sdk")
-        bitbake core-image-minimal -c populate_sdk
-        ;;
-    "taskexp")
-        bitbake -u taskexp -g core-image-minimal
-        ;;
-esac
+if [ "$ACTION" == "all" ] || [ "$ACTION" == "fetchall" ]; then
+    bitbake core-image-minimal -c fetchall
+fi
+if [ "$ACTION" == "all" ] || [ "$ACTION" == "buildall" ]; then
+    bitbake core-image-minimal
+fi
+if [ "$ACTION" == "all" ] || [ "$ACTION" == "populate_sdk" ]; then
+    bitbake core-image-minimal -c populate_sdk
+fi
+if [ "$ACTION" == "all" ] || [ "$ACTION" == "taskexp" ]; then
+    bitbake -u taskexp -g core-image-minimal
+fi
 
-RC=$?
 cd "$TOPDIR"
-exit $RC
