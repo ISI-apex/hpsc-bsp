f2d8d3d1cd0a0e106eb025b87977447afe5a2bad
Connor Imes <cimes@isi.edu>
2019-02-13 08:59:27 -0500

    build-hpsc-bsp: don't stage/package cpio filesystem image anymore

--- build-hpsc-bsp.sh
+++ build-hpsc-bsp.sh
@@ -36,9 +36,7 @@ BSP_ARTIFACTS_HPPS=("${POKY_IMAGE_DIR}/arm-trusted-firmware.bin"
                     "${POKY_IMAGE_DIR}/u-boot.bin"
                     "${POKY_IMAGE_DIR}/hpsc.dtb"
                     "${POKY_IMAGE_DIR}/Image.gz"
-                    "${POKY_IMAGE_DIR}/core-image-hpsc-hpsc-chiplet.cpio.gz.u-boot"
-                    # plain cpio file not used by run-qemu, but used elsewhere
-                    "${POKY_IMAGE_DIR}/core-image-hpsc-hpsc-chiplet.cpio")
+                    "${POKY_IMAGE_DIR}/core-image-hpsc-hpsc-chiplet.cpio.gz.u-boot")
 BSP_ARTIFACTS_AARCH64_UTIL=("${UTILS_DIR}/linux/mboxtester"
                             "${UTILS_DIR}/linux/wdtester")
 BSP_ARTIFACTS_HOST_UTIL=("${UTILS_DIR}/host/qemu-nand-creator"
