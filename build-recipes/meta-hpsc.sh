dbc17d65f6859240d0eeaee20c114427e2ee0c96
Connor Imes <cimes@isi.edu>
2019-03-08 17:21:58 -0500

    meta-hpsc: upgrade to rev: 6cf44b76935527d352b252954814ea339d91a2ee

--- build-recipes/meta-hpsc.sh
+++ build-recipes/meta-hpsc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_REPO="https://github.com/ISI-apex/meta-hpsc.git"
-export GIT_REV=31ec6f6508762a55bee86156caa059c307b8fdca
+export GIT_REV=6cf44b76935527d352b252954814ea339d91a2ee
 export GIT_BRANCH="hpsc"
 
 # Yocto layers aren't built
