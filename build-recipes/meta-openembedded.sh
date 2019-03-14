ac956421633e60d25e18f18722c7ebad24557404
Connor Imes <cimes@isi.edu>
2019-03-14 08:41:12 -0400

    meta-openembedded: upgrade to 6ef9657068492d4644079c88f2adee9c3cac9543

--- build-recipes/meta-openembedded.sh
+++ build-recipes/meta-openembedded.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_REPO="https://github.com/openembedded/meta-openembedded.git"
-export GIT_REV=6094ae18c8a35e5cc9998ac39869390d7f3bb1e2
+export GIT_REV=6ef9657068492d4644079c88f2adee9c3cac9543
 export GIT_BRANCH="thud"
 
 # Yocto layers aren't built
