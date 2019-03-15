8adf2c5987b308b752da5015e9d59b54efb7b562
Connor Imes <cimes@isi.edu>
2019-03-15 12:02:05 -0400

    build-recipes: add hpsc-yocto

---
+++ build-recipes/hpsc-yocto.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+#
+# A meta recipe to construct the HPSC Yocto configuration
+#
+# Implicitly depends on:
+#  poky
+#  meta-openembedded
+#  meta-hpsc
+#
+
+# The only content in our src dir are the sources downloaded in do_late_fetch.
+# We don't need/want to copy this large directory to the work dir.
+export DO_BUILD_OUT_OF_SOURCE=1
+
+# Builds take a long time, but OpenEmbedded is very good at incremental builds,
+# so don't let the build directory be cleaned unless user forces it.
+export DO_CLEAN_AFTER_FETCH=0
+
+
+DL_DIR="${REC_SRC_DIR}/poky_dl"
+BUILD_DIR="${REC_WORK_DIR}/poky_build"
+
+POKY_DIR=$(get_dependency_src "poky")
+META_OE_DIR=$(get_dependency_src "meta-openembedded")
+META_HPSC_DIR=$(get_dependency_src "meta-hpsc")
+LAYERS=("${META_OE_DIR}/meta-oe"
+        "${META_OE_DIR}/meta-python"
+        "${META_HPSC_DIR}/meta-hpsc-bsp")
+
+function yocto_setup()
+{
+    local LAYER_ARGS=()
+    for l in "${LAYERS[@]}"; do
+        LAYER_ARGS+=("-l" "$l")
+    done
+    source "${REC_UTIL_DIR}/configure-env.sh" -d "${DL_DIR}" -b "${BUILD_DIR}" \
+                                              -p "${POKY_DIR}" "${LAYER_ARGS[@]}"
+}
+
+# test-only recipes must be fetched/built independently of rootfs image
+TEST_RECIPES=()
+# Parse test module JSON file to get its recipe dependencies
+function get_json_dependent_recipes()
+{
+    python -c "import sys, json
+json = json.load(sys.stdin)
+print '\n'.join([json[j]['pkg'] for j in json])" < "$1"
+}
+# Search layer for test module JSON files
+function find_layer_test_mod_jsons()
+{
+    # per Yocto documentation, test cases always in: lib/oeqa/runtime/cases
+    local tcdir="$1/lib/oeqa/runtime/cases"
+    if test -d "$tcdir"; then
+        find "$tcdir" -maxdepth 1 -name "*.json"
+    fi
+}
+# Search layer for JSON-specified test dependencies
+function find_layer_test_recipes_for_suites()
+{
+    local laydir=$1
+    shift
+    # grep expression replaces "suite1 suite2 ..." with "suite1|suite2|..." so
+    # JSONS only matches specified test modules
+    # NOTE: assumes no newline characters or leading/trailing whitespace
+    local suite_e=$(echo "$*" | tr "[:blank:]" "|")
+    if [ -z "$(echo "$suite_e" | tr -d "|")" ]; then
+        # no test modules (or specified as empty string or whitespace) causes
+        # function to match on all JSON files when we actually want to filter
+        return 
+    fi
+    local JSONS=($(find_layer_test_mod_jsons "$laydir" | grep -E "$suite_e"))
+    local RECIPES=()
+    for f in "${JSONS[@]}"; do
+        RECIPES+=($(get_json_dependent_recipes "$f"))
+    done
+    echo "${RECIPES[@]}" # note: may contain duplicate entries
+}
+function yocto_get_test_recipes()
+{
+    local TEST_SUITES=($(grep TEST_SUITES conf/local.conf | cut -d'=' -f2- | tr -d '"' | xargs))
+    echo "Found test suites: (${TEST_SUITES[*]})"
+    local LAYERS=($(bitbake-layers show-layers | tail -n +4 | awk '{print $2}'))
+    local TRS=()
+    for l in "${LAYERS[@]}"; do
+        TRS+=($(find_layer_test_recipes_for_suites "$l" "${TEST_SUITES[@]}"))
+    done
+    # filter duplicate entries
+    TEST_RECIPES=($(echo "${TRS[@]}" | tr ' ' '\n' | sort -u))
+    echo "Found test recipes: (${TEST_RECIPES[*]})"
+}
+
+HPSC_YOCTO_INITIALIZED=0
+function yocto_maybe_init_env()
+{
+    if [ $HPSC_YOCTO_INITIALIZED -eq 0 ]; then
+        yocto_setup
+        yocto_get_test_recipes
+        HPSC_YOCTO_INITIALIZED=1
+    fi
+}
+
+function do_late_fetch()
+{
+    yocto_maybe_init_env
+    bitbake core-image-hpsc "${TEST_RECIPES[@]}" --runall="fetch"
+    bitbake core-image-hpsc -c populate_sdk --runall="fetch"
+    bitbake core-image-hpsc -c testimage --runall="fetch"
+}
+
+function do_build()
+{
+    yocto_maybe_init_env
+    # subshell is so BB_NO_NETWORK isn't remembered
+    (
+        echo "Building with BB_NO_NETWORK=1"
+        export BB_NO_NETWORK=1
+        bitbake core-image-hpsc "${TEST_RECIPES[@]}"
+        bitbake core-image-hpsc -c populate_sdk
+    )
+}
+
---
+++ build-recipes/hpsc-yocto/configure-env.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#
+# Configure Yocto/poky environment.
+#
+# After sourcing, the working directory will be the poky build directory and
+# the caller's environment will be set by poky's 'oe-init-build-env' script.
+#
+
+# This script is sourced, it must not set -e
+# set -e
+
+TEST_MODULES=(perl ping scp ssh date mpi openmp pthreads shm_standalone)
+
+function usage()
+{
+    echo "Usage: $0 -d DIR -b DIR -p DIR [-l DIR]+ [-h]"
+    echo "    -d DIR: set the download directory (use a full path)"
+    echo "    -b DIR: set the build directory (use a full path)"
+    echo "    -p DIR: set the poky source directory (use a full path)"
+    echo "    -l DIR: add a layer directory (use a full path)"
+    echo "    -h: show this message and exit"
+}
+
+# Script options
+DL_DIR=""
+BUILD_DIR=""
+POKY_DIR=""
+LAYER_DIRS=()
+# parse options
+OPTIND=1 # reset since we're probably being sourced
+while getopts "d:b:p:l:h?" o; do
+    case "$o" in
+        d)
+            DL_DIR="${OPTARG}"
+            ;;
+        b)
+            BUILD_DIR="${OPTARG}"
+            ;;
+        p)
+            POKY_DIR="${OPTARG}"
+            ;;
+        l)
+            LAYER_DIRS+=("${OPTARG}")
+            ;;
+        h)
+            usage
+            return
+            ;;
+        *)
+            echo "Unknown option"
+            usage
+            return 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+if [ -z "$DL_DIR" ] || [ -z "$BUILD_DIR" ] || [ -z "$POKY_DIR" ]; then
+    usage
+fi
+
+# poky's sanity checker tries to reach example.com unless we force it offline
+export BB_NO_NETWORK=1
+# create build directory and cd to it
+source "${POKY_DIR}/oe-init-build-env" "$BUILD_DIR" || return $?
+# configure layers
+for layer in "${LAYER_DIRS[@]}"; do
+    bitbake-layers add-layer "$layer" || return $?
+done
+unset BB_NO_NETWORK
+
+# configure local.conf
+function conf_replace_or_append()
+{
+    local line=$1
+    local file="conf/local.conf"
+    local key=$(echo "$line" | awk '{print $1}')
+    # support assignment types: ?=, +=, or =
+    # Using '@' instead of '/' in sed so paths can be values
+    grep -q "^$key ?*+*=" "$file" && sed -i "s@^${key} .*@${line}@" "$file" || \
+        echo "$line" >> "$file"
+}
+conf_replace_or_append "MACHINE ?= \"hpsc-chiplet\""
+conf_replace_or_append "DL_DIR ?= \"${DL_DIR}\""
+conf_replace_or_append "FORTRAN_forcevariable = \",fortran\""
+# the following commands are needed for enabling runtime tests
+conf_replace_or_append "INHERIT_append += \" testimage\""
+conf_replace_or_append "TEST_TARGET = \"simpleremote\""
+conf_replace_or_append "TEST_SERVER_IP = \"$(hostname -I | cut -d ' ' -f 1)\""
+conf_replace_or_append "TEST_TARGET_IP = \"127.0.0.1:3040\""
+conf_replace_or_append "IMAGE_FSTYPES_append += \" cpio.gz\""
+conf_replace_or_append "TEST_SUITES += \"${TEST_MODULES[*]}\""
