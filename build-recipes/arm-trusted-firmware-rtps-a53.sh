37415eaab0feae065064d6aa770fbace5f9f9618
Connor Imes <cimes@isi.edu>
2019-03-18 14:33:23 -0400

    arm-trusted-firmware-rtps-a53: upgrade to rev: f08a96f

--- build-recipes/arm-trusted-firmware-rtps-a53.sh
+++ build-recipes/arm-trusted-firmware-rtps-a53.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_REPO="https://github.com/ISI-apex/arm-trusted-firmware.git"
-export GIT_REV=d6dc7ef1f6e0e9b07fd29748aafb41899a7fdcdf
+export GIT_REV=f08a96ff2c5e5177b0902a1a02ffc4c3030a5bee
 export GIT_BRANCH="hpsc"
 
 function do_build()
