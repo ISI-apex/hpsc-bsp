ISI-apex/hpsc-bsp

commit 21210c1a25fa8399c67473038e0adb097a5f33bf

subdirs per subsystem, and for sdk and ssw
Implementation: simply changed dashes to slashes:
u-boot-rtps-r52 -> ssw/rts/r52/u-boot

Names of recipes correspond to names of repositories.

The SDK vs SSW split is imperfect: Yocto is not split, so it introduces
a couple cross-references marked with comments.

The current assumption is that rtems-source-builder and rtems-tools (in
their built binary form) will be re-usable for building RTEMS for all
cores (for {R52, M4}, this is likely, for {R52, M4, A53}, this is
unlikely, but in case it is needed, then we would simply have
sdk/{arm,arm64}/rtems_source_builder).

utils/configure-hpsc-yocto-env.sh
@@ -56,18 +56,21 @@ if [ "$IS_FETCH" -ne 0 ]; then
                                                  || return $?
 fi
 
-# TODO: mimicks build-recipes/hpsc-yocto-hpps.sh... could easily get out of sync
+# TODO: mimicks build-recipes/ssw/hpps/yocto.sh... could easily get out of sync
 
 FULL_WD="${PWD}/${WORKING_DIR}"
+YOCTO_SUBDIR="ssw/hpps/yocto"
+YOCTO_SRC_DIR="${FULL_WD}/src/${YOCTO_SUBDIR}"
+YOCTO_WRK_DIR="${FULL_WD}/work/${YOCTO_SUBDIR}"
 
-mkdir -p "${FULL_WD}/src/hpsc-yocto-hpps" "${FULL_WD}/work/hpsc-yocto-hpps"
+mkdir -p "${YOCTO_SRC_DIR}" "${YOCTO_WORK_DIR}"
 
-DL_DIR="${FULL_WD}/src/hpsc-yocto-hpps/poky_dl"
-BUILD_DIR="${FULL_WD}/work/hpsc-yocto-hpps/poky_build"
+DL_DIR="${YOCTO_SRC_DIR}/poky_dl"
+BUILD_DIR="${YOCTO_WORK_DIR}/poky_build"
 
-POKY_DIR="${FULL_WD}/src/poky"
-META_OE_DIR="${FULL_WD}/src/meta-openembedded"
-META_HPSC_DIR="${FULL_WD}/src/meta-hpsc"
+POKY_DIR="${YOCTO_SRC_DIR}/poky"
+META_OE_DIR="${YOCTO_SRC_DIR}/meta-openembedded"
+META_HPSC_DIR="${YOCTO_SRC_DIR}/meta-hpsc"
 LAYERS=("${META_OE_DIR}/meta-oe"
         "${META_OE_DIR}/meta-python"
         "${META_HPSC_DIR}/meta-hpsc-bsp")
@@ -75,7 +78,7 @@ LAYERS=("${META_OE_DIR}/meta-oe"
 for l in "${LAYERS[@]}"; do
     LAYER_ARGS+=("-l" "$l")
 done
-source build-recipes/hpsc-yocto-hpps/configure-env.sh -d "${DL_DIR}" \
-                                                      -b "${BUILD_DIR}" \
-                                                      -p "${POKY_DIR}" \
-                                                      "${LAYER_ARGS[@]}"
+source build-recipes/${YOCTO_SUBDIR}/utils/configure-env.sh -d "${DL_DIR}" \
+                                                            -b "${BUILD_DIR}" \
+                                                            -p "${POKY_DIR}" \
+                                                            "${LAYER_ARGS[@]}"

utils/upgrade-recipe-yocto-all.sh
@@ -6,10 +6,10 @@ set -e
 
 LAYBRANCH=hpsc
 RECIPES=(
-    arm-trusted-firmware
-    u-boot-hpps
-    linux-hpsc # linux takes awhile to upgrade (lots of native dependencies)
-    hpsc-utils
+    ssw/hpps/arm-trusted-firmware
+    ssw/hpps/u-boot
+    ssw/hpps/linux # linux takes awhile to upgrade (lots of native dependencies)
+    ssw/hpsc-utils
 )
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"

build-hpsc-other.sh
@@ -7,16 +7,16 @@
 set -e
 
 # The recipes managed by this script
-RECIPES=("gcc-arm-none-eabi"
-         "hpsc-sdk-tools"
-         "hpsc-baremetal"
-         "arm-trusted-firmware-rtps-a53"
-         "u-boot-rtps-r52"
-         "u-boot-rtps-a53"
-         "qemu"
-         "qemu-devicetrees"
-         "hpsc-utils"
-         "hpsc-eclipse")
+RECIPES=("sdk/gcc-arm-none-eabi"
+         "sdk/hpsc-sdk-tools"
+         "sdk/qemu"
+         "sdk/qemu-devicetrees"
+         "ssw/hpsc-baremetal"
+         "ssw/rtps/a53/arm-trusted-firmware"
+         "ssw/rtps/a53/u-boot"
+         "ssw/rtps/r52/u-boot"
+         "ssw/hpsc-utils"
+         "sdk/hpsc-eclipse")
 
 function usage()
 {

build-hpsc-rtems.sh
@@ -3,10 +3,10 @@
 # Fail-fast
 set -e
 
-RECIPES=("rtems-tools"
-         "rtems-source-builder"
-         "rtems-rtps-r52"
-         "hpsc-rtems-rtps-r52")
+RECIPES=("sdk/rtems-tools"
+         "sdk/rtems-source-builder"
+         "ssw/rtps/r52/rtems"
+         "ssw/rtps/r52/hpsc-rtems")
 
 function usage()
 {

build-hpsc-yocto.sh
@@ -3,10 +3,10 @@
 # Fail-fast
 set -e
 
-RECIPES=("poky"
-         "meta-openembedded"
-         "meta-hpsc"
-         "hpsc-yocto-hpps")
+RECIPES=("ssw/hpps/yocto/poky"
+         "ssw/hpps/yocto/meta-openembedded"
+         "ssw/hpps/yocto/meta-hpsc"
+         "ssw/hpps/yocto")
 
 function usage()
 {

build-recipe.sh
@@ -97,7 +97,7 @@ function build_set_recipe_env()
     # variables for recipes
     export ENV_WORKING_DIR="${PWD}"
     export ENV_DEPLOY_DIR="${ENV_WORKING_DIR}/deploy"
-    export REC_UTIL_DIR="${REC_DIR}/${recname}"
+    export REC_UTIL_DIR="${REC_DIR}/${recname}/utils"
     export REC_SRC_DIR="${ENV_WORKING_DIR}/src/${recname}"
     export REC_WORK_DIR="${ENV_WORKING_DIR}/work/${recname}"
     export REC_ENV_DIR="${ENV_WORKING_DIR}/env" # shared b/w recipes, shhh... ;)
@@ -160,6 +160,7 @@ function build_lifecycle()
         if [ ! -d "$REC_WORK_DIR" ]; then
             if [ "$DO_BUILD_OUT_OF_SOURCE" -eq 0 ]; then
                 echo "$recname: extract"
+                mkdir -p "$(dirname $REC_WORK_DIR)"
                 cp -r "$REC_SRC_DIR" "$REC_WORK_DIR"
             else
                 mkdir -p "$REC_WORK_DIR"

build-recipes/ENV.sh
@@ -189,7 +189,7 @@ function ENV_check_bm_toolchain()
 {
     if ! which arm-none-eabi-gcc > /dev/null 2>&1; then
         echo "Error: Bare metal cross compiler 'arm-none-eabi-gcc' is not on PATH"
-        echo "  Ensure that recipe has DEPENDS_ENVIRONMENT on 'gcc-arm-none-eabi' and that it is built"
+        echo "  Ensure that recipe has DEPENDS_ENVIRONMENT on 'sdk/gcc-arm-none-eabi' and that it is built"
         return 1
     fi
 }
@@ -199,7 +199,7 @@ function ENV_check_yocto_hpps_sdk()
 {
     if [ -z "$YOCTO_HPPS_SDK" ] || [ ! -d "$YOCTO_HPPS_SDK" ]; then
         echo "Error: YOCTO_HPPS_SDK not found: $YOCTO_HPPS_SDK"
-        echo "  Ensure that recipe has DEPENDS_ENVIRONMENT on 'hpsc-yocto-hpps' and that it is built"
+        echo "  Ensure that recipe has DEPENDS_ENVIRONMENT on 'ssw/hpps/yocto' and that it is built"
         return 1
     fi
 }
@@ -209,7 +209,7 @@ function ENV_check_rsb_toolchain()
 {
     if ! which arm-rtems5-gcc > /dev/null 2>&1; then
         echo "Error: RTEMS cross compiler 'arm-rtems5-gcc' is not on PATH"
-        echo "  Ensure that recipe has DEPENDS_ENVIRONMENT on 'rtems-source-builder' and that it is built"
+        echo "  Ensure that recipe has DEPENDS_ENVIRONMENT on 'sdk/rtems-source-builder' and that it is built"
         return 1
     fi
 }
@@ -219,7 +219,7 @@ function ENV_check_rtems_r52_sdk()
 {
     if [ -z "$RTEMS_RTPS_R52_BSP" ] || [ ! -d "$RTEMS_RTPS_R52_BSP" ]; then
         echo "Error: RTEMS_RTPS_R52_BSP not found: $RTEMS_RTPS_R52_BSP"
-        echo "  Ensure that recipe has DEPENDS_ENVIRONMENT on 'rtems-rtps-r52' and that it is built"
+        echo "  Ensure that recipe has DEPENDS_ENVIRONMENT on 'ssw/rtps/r52/rtems' and that it is built"
         return 1
     fi
 }

commit 3f12b0fb09189af3539c1fd2b9cde9b7a18b4dde

utils: yocto: fix intra-yocto rec names and typo
Fixes for things broken by 21210c1.

utils/configure-hpsc-yocto-env.sh
@@ -50,9 +50,9 @@ cd "$BSP_DIR"
 IS_FETCH=${IS_FETCH:-1}
 # clone poky and the layers we configure
 if [ "$IS_FETCH" -ne 0 ]; then
-    ./build-recipe.sh -w "$WORKING_DIR" -a fetch -r poky \
-                                                 -r meta-openembedded \
-                                                 -r meta-hpsc \
+    ./build-recipe.sh -w "$WORKING_DIR" -a fetch -r ssw/hpps/yocto/poky \
+                                                 -r ssw/hpps/yocto/meta-openembedded \
+                                                 -r ssw/hpps/yocto/meta-hpsc \
                                                  || return $?
 fi
 
@@ -61,7 +61,7 @@ fi
 FULL_WD="${PWD}/${WORKING_DIR}"
 YOCTO_SUBDIR="ssw/hpps/yocto"
 YOCTO_SRC_DIR="${FULL_WD}/src/${YOCTO_SUBDIR}"
-YOCTO_WRK_DIR="${FULL_WD}/work/${YOCTO_SUBDIR}"
+YOCTO_WORK_DIR="${FULL_WD}/work/${YOCTO_SUBDIR}"
 
 mkdir -p "${YOCTO_SRC_DIR}" "${YOCTO_WORK_DIR}"
 

utils/upgrade-recipe-yocto-all.sh
@@ -5,11 +5,13 @@
 set -e
 
 LAYBRANCH=hpsc
+
+# recipe names from yocto layers
 RECIPES=(
-    ssw/hpps/arm-trusted-firmware
-    ssw/hpps/u-boot
-    ssw/hpps/linux # linux takes awhile to upgrade (lots of native dependencies)
-    ssw/hpsc-utils
+    arm-trusted-firmware
+    u-boot-hpps
+    linux-hpsc # linux takes awhile to upgrade (lots of native dependencies)
+    hpsc-utils
 )
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"