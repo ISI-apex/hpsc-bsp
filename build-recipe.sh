5a54d00dc16e8c648eacb74e424bade4cd822fbe
Connor Imes <cimes@isi.edu>
2019-06-06 16:14:30 -0400

    build-recipe: factor build steps into functions

--- build-recipe.sh
+++ build-recipe.sh
@@ -94,7 +94,7 @@ function build_recipe_export_env_base()
 {
     local recname=$1
     # recipes extend the ENV script
-    source "${REC_DIR}/ENV.sh"
+    source "${REC_DIR}/ENV.sh" || return $?
     # variables for recipes
     export ENV_WORKING_DIR="${PWD}"
     export ENV_DEPLOY_DIR="${ENV_WORKING_DIR}/deploy"
@@ -109,86 +109,168 @@ function build_recipe_export_env_with_deps()
 {
     local recname=$1
     # we really just want DEPENDS_ENVIRONMENT to begin with
-    build_recipe_export_env_base "$recname"
+    build_recipe_export_env_base "$recname" || return $?
     # cache DEPENDS_ENVIRONMENT since it will be overridden by dependencies
     IFS=':' read -ra DEP_ENV_CACHE <<< "$DEPENDS_ENVIRONMENT"
     for de in "${DEP_ENV_CACHE[@]}"; do
         echo "$recname: depends: $de"
-        build_recipe_export_env_base "$de"
+        build_recipe_export_env_base "$de" || return $?
     done
     # now that we have exported from dependencies, we need our own environment
     build_recipe_export_env_base "$recname"
+}
+
+function build_step_fetch()
+{
+    echo "$1: fetch"
+    cd "$REC_SRC_DIR" && do_fetch
+}
+
+function build_step_post_fetch()
+{
+    echo "$1: post_fetch"
+    cd "$REC_SRC_DIR" && do_post_fetch
+}
+
+function build_step_late_fetch()
+{
+    echo "$1: late_fetch"
+    cd "$REC_WORK_DIR" && do_late_fetch
+}
+
+function build_step_toolchain_uninstall()
+{
+    if [ "$DO_FETCH_ONLY" -ne 0 ]; then
+        echo "$1: toolchain_uninstall"
+        cd "$REC_SRC_DIR" && do_toolchain_uninstall
+    elif [ -d "$REC_WORK_DIR" ]; then
+        echo "$1: toolchain_uninstall"
+        cd "$REC_WORK_DIR" && do_toolchain_uninstall
+    fi # else nowhere to run uninstall from
+}
+
+function build_step_undeploy()
+{
+    if [ "$DO_FETCH_ONLY" -ne 0 ]; then
+        echo "$1: undeploy"
+        cd "$REC_SRC_DIR" && do_undeploy
+    elif [ -d "$REC_WORK_DIR" ]; then
+        echo "$1: undeploy"
+        cd "$REC_WORK_DIR" && do_undeploy
+    fi # else nowhere to run undeploy from
+}
+
+# TODO: Not a real build step
+function build_step_clean()
+{
+    echo "$1: clean"
+    cd "$ENV_WORKING_DIR" && rm -rf "$REC_WORK_DIR"
+}
+
+# TODO: Not a real build step
+function build_step_extract()
+{
+    echo "$1: extract"
+    mkdir -p "$(dirname "$REC_WORK_DIR")" && \
+    cp -r "$REC_SRC_DIR" "$REC_WORK_DIR"
+}
+
+function build_step_build()
+{
+    if [ ! -d "$REC_WORK_DIR" ]; then
+        echo "$1: Error: must 'fetch' before 'build'"
+        return 1
+    fi
+    echo "$1: build"
+    cd "$REC_WORK_DIR" && do_build
+}
+
+function build_step_test()
+{
+    if [ ! -d "$REC_WORK_DIR" ]; then
+        echo "$1: Error: must 'fetch' before 'test'"
+        return 1
+    fi
+    echo "$1: test"
+    cd "$REC_WORK_DIR" && do_test
+}
+
+function build_step_deploy()
+{
+    local dir
+    if [ "$DO_FETCH_ONLY" -ne 0 ]; then
+        dir="$REC_SRC_DIR"
+    else
+        if [ ! -d "$REC_WORK_DIR" ]; then
+            echo "$1: Error: must 'fetch' before 'deploy'"
+            return 1
+        fi
+        dir="$REC_WORK_DIR"
+    fi
+    echo "$1: deploy"
+    cd "$dir" && do_deploy
+}
+
+function build_step_toolchain_install()
+{
+    local dir
+    if [ "$DO_FETCH_ONLY" -ne 0 ]; then
+        dir="$REC_SRC_DIR"
+    else
+        if [ ! -d "$REC_WORK_DIR" ]; then
+            echo "$1: Error: must 'fetch' before 'toolchain_install'"
+            return 1
+        fi
+        dir="$REC_WORK_DIR"
+    fi
+    echo "$1: toolchain_install"
+    cd "$dir" && do_toolchain_install
 }
 
 function build_lifecycle()
 {
     local recname=$1
 
+    # setup build environment
     build_recipe_export_env_with_deps "$recname"
 
     # fetch is broken up to allow custom clean and extract
     if [ $IS_FETCH -ne 0 ]; then
         mkdir -p "$REC_SRC_DIR"
-        echo "$recname: fetch"
-        cd "$REC_SRC_DIR"
-        do_fetch
-        echo "$recname: post_fetch"
-        cd "$REC_SRC_DIR"
-        do_post_fetch
+        build_step_fetch "$recname"
+        build_step_post_fetch "$recname"
     fi
 
     # uninstall/undeploy on clean request or after fetch
     if [ $IS_CLEAN -ne 0 ] || [ $IS_FETCH -ne 0 ]; then
-        if [ "$DO_FETCH_ONLY" -ne 0 ]; then
-            echo "$recname: toolchain_uninstall"
-            cd "$REC_SRC_DIR"
-            do_toolchain_uninstall
-            echo "$recname: undeploy"
-            cd "$REC_SRC_DIR"
-            do_undeploy
-        elif [ -d "$REC_WORK_DIR" ]; then
-            echo "$recname: toolchain_uninstall"
-            cd "$REC_WORK_DIR"
-            do_toolchain_uninstall
-            echo "$recname: undeploy"
-            cd "$REC_WORK_DIR"
-            do_undeploy
-        fi # else nowhere to run uninstall/undeploy from
+        build_step_toolchain_uninstall "$recname"
+        build_step_undeploy "$recname"
     fi
 
+    # some recipes are source-only
     if [ "$DO_FETCH_ONLY" -eq 0 ]; then
         # clean if requested or clean-after-fetch not overridden by recipe
         if [ $IS_CLEAN -ne 0 ] || 
            [[ $IS_FETCH -ne 0 && "$DO_CLEAN_AFTER_FETCH" -eq 1 ]]; then
-            echo "$recname: clean"
-            cd "$ENV_WORKING_DIR"
-            rm -rf "$REC_WORK_DIR"
+            build_step_clean "$recname"
         fi
+
         # extract to (or create) work dir
         if [ ! -d "$REC_WORK_DIR" ]; then
             if [ "$DO_BUILD_OUT_OF_SOURCE" -eq 0 ]; then
-                echo "$recname: extract"
-                mkdir -p "$(dirname "$REC_WORK_DIR")"
-                cp -r "$REC_SRC_DIR" "$REC_WORK_DIR"
+                build_step_extract "$recname"
             else
                 mkdir -p "$REC_WORK_DIR"
             fi
         fi
+
+        # late fetch is for fetching that requires a work dir first
         if [ $IS_FETCH -ne 0 ]; then
-            # late fetch is for fetching that requires a work dir first
-            echo "$recname: late_fetch"
-            cd "$REC_WORK_DIR"
-            do_late_fetch
+            build_step_late_fetch "$recname"
         fi
 
         if [ $IS_BUILD -ne 0 ]; then
-            if [ ! -d "$REC_WORK_DIR" ]; then
-                echo "$recname: Error: must 'fetch' before 'build'"
-                return 1
-            fi
-            echo "$recname: build"
-            cd "$REC_WORK_DIR"
-            do_build && RC=0 || RC=$?
+            build_step_build "$recname" && RC=0 || RC=$?
             if [ $RC -eq 0 ]; then
                 echo "$recname: build successful"
             else
@@ -198,42 +280,16 @@ function build_lifecycle()
         fi
 
         if [ $IS_TEST -ne 0 ]; then
-            if [ ! -d "$REC_WORK_DIR" ]; then
-                echo "$recname: Error: must 'fetch' before 'test'"
-                return 1
-            fi
-            echo "$recname: test"
-            cd "$REC_WORK_DIR"
-            do_test
+            build_step_test "$recname"
         fi
     fi
 
     if [ $IS_DEPLOY -ne 0 ]; then
-        if [ "$DO_FETCH_ONLY" -ne 0 ]; then
-            cd "$REC_SRC_DIR"
-        else
-            if [ ! -d "$REC_WORK_DIR" ]; then
-                echo "$recname: Error: must 'fetch' before 'deploy'"
-                return 1
-            fi
-            cd "$REC_WORK_DIR"
-        fi
-        echo "$recname: deploy"
-        do_deploy
+        build_step_deploy "$recname"
     fi
 
     if [ $IS_TOOLCHAIN_INSTALL -ne 0 ]; then
-        if [ "$DO_FETCH_ONLY" -ne 0 ]; then
-            cd "$REC_SRC_DIR"
-        else
-            if [ ! -d "$REC_WORK_DIR" ]; then
-                echo "$recname: Error: must 'fetch' before 'toolchain_install'"
-                return 1
-            fi
-            cd "$REC_WORK_DIR"
-        fi
-        echo "$recname: toolchain_install"
-        do_toolchain_install
+        build_step_toolchain_install "$recname"
     fi
 }
 
