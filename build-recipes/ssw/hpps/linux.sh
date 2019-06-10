226b637889cc9f9aa41fbc9d2052a5e725d83a0f
Connor Imes <cimes@isi.edu>
2019-06-10 12:01:57 -0400

    ssw/hpps/linux: upgrade to rev: 6964cd3

--- build-recipes/ssw/hpps/linux.sh
+++ build-recipes/ssw/hpps/linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_REPO="https://github.com/ISI-apex/linux-hpsc.git"
-export GIT_REV=08bd4b75dddeaa8fb41aee4720938cc0da317b0e
+export GIT_REV=6964cd371ec788e906d71f634a4deed76d0951ec
 export GIT_BRANCH="hpsc"
 
 export DO_FETCH_ONLY=1
