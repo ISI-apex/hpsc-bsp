36aa16afd083fb5ad0a5971c713a9953c71f3369
Connor Imes <cimes@isi.edu>
2019-07-11 15:08:00 -0400

    sdk/hpsc-eclipse: temporarily disabling linuxtools plugins
    The specified versions are no longer available online.
    We will need to update versions, but also fix the problem of versions
    becoming outdated and unavailable.
    See issue HPSC/HPSC#78

--- build-recipes/sdk/hpsc-eclipse.sh
+++ build-recipes/sdk/hpsc-eclipse.sh
@@ -20,14 +20,15 @@ ECLIPSE_PLUGIN_IUS=(org.yocto.doc.feature.group/1.4.1.201901082310
                     ilg.gnumcueclipse.managedbuild.cross.arm/2.6.4.201901011632
                     ilg.gnumcueclipse.debug.core/1.2.2.201901011632
                     ilg.gnumcueclipse.templates.cortexm.feature.feature.group/1.4.4.201901011632
-                    org.eclipse.linuxtools.perf.feature.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.perf.remote.feature.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.perf.feature.source.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.perf.remote.feature.source.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.profiling.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.profiling.remote.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.profiling.source.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.profiling.remote.source.feature.group/7.1.0.201812121718)
+                    # org.eclipse.linuxtools.perf.feature.feature.group/7.1.0.201812121718
+                    # org.eclipse.linuxtools.perf.remote.feature.feature.group/7.1.0.201812121718
+                    # org.eclipse.linuxtools.perf.feature.source.feature.group/7.1.0.201812121718
+                    # org.eclipse.linuxtools.perf.remote.feature.source.feature.group/7.1.0.201812121718
+                    # org.eclipse.linuxtools.profiling.feature.group/7.1.0.201812121718
+                    # org.eclipse.linuxtools.profiling.remote.feature.group/7.1.0.201812121718
+                    # org.eclipse.linuxtools.profiling.source.feature.group/7.1.0.201812121718
+                    # org.eclipse.linuxtools.profiling.remote.source.feature.group/7.1.0.201812121718
+)
 
 ECLIPSE_DIR=eclipse
 ECLIPSE_HPSC=hpsc-eclipse.tar.gz
