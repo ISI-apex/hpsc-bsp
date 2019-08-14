2d99e3e0f285c4bd976d3b1f5308ef2416222340
Connor Imes <cimes@isi.edu>
2019-08-14 08:49:12 -0400

    ssw/hpps/yocto/poky: upgrade to rev: cb26830 (thud-20.0.3)

--- build-recipes/ssw/hpps/yocto/poky.sh
+++ build-recipes/ssw/hpps/yocto/poky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_REPO="https://git.yoctoproject.org/git/poky"
-export GIT_REV=e7f0177ef3b6e06b8bc1722fca0241fef08a1530 # thud-20.0.2 tag
+export GIT_REV=cb26830f765f03309c0663352cc5849491271be8 # thud-20.0.3 tag
 export GIT_BRANCH="thud"
 
 # poky is built by hpsc-yocto, which integrates layers
