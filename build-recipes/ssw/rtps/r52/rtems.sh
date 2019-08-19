ad26b5858dd08db2543464a3fc24877fb1248936
Connor Imes <cimes@isi.edu>
2019-08-19 16:29:16 -0400

    ssw/rtps/r52/rtems: upgrade to rev: 88b71f4

--- build-recipes/ssw/rtps/r52/rtems.sh
+++ build-recipes/ssw/rtps/r52/rtems.sh
@@ -5,7 +5,7 @@
 #
 
 export GIT_REPO="https://github.com/ISI-apex/rtems.git"
-export GIT_REV=ae5f7994fcff1e312b90c1ac915b45abcdd4fbf7
+export GIT_REV=88b71f4c7f9814286be16012376e8fee287e1203
 export GIT_BRANCH=hpsc-1.2
 
 export DEPENDS_ENVIRONMENT="sdk/rtems-source-builder" # exports PATH
