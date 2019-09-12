3481b7ca6cffe78ec6e22385bb0e803cccd470b3
Connor Imes <cimes@isi.edu>
2019-09-12 08:57:59 -0400

    build-recipe-env: move more work into functions
    Reduces conflicts on variables with sourcing scripts

--- build-recipes/build-recipe-env.sh
+++ build-recipes/build-recipe-env.sh
@@ -9,7 +9,6 @@
 
 # This script is sourced, it must not set -e
 # set -e
-THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 function usage()
 {
@@ -19,64 +18,72 @@ function usage()
     echo "    -h: show this message and exit"
 }
 
-# Script options
-RECIPE=""
-WORKING_DIR=""
-OPTIND=1 # reset since we're probably being sourced
-while getopts "r:w:h?" o; do
-    case "$o" in
-        r)
-            RECIPE="${OPTARG}"
-            ;;
-        w)
-            WORKING_DIR="${OPTARG}"
-            ;;
-        h)
-            usage
-            return
-            ;;
-        *)
-            echo "Unknown option"
-            usage
-            return 1
-            ;;
-    esac
-done
-shift $((OPTIND-1))
-if [ -z "$RECIPE" ] || [ -z "$WORKING_DIR" ]; then
-    usage
-    return 1
-fi
-cd "$WORKING_DIR" || return $?
-
 function build_recipe_export_env_base()
 {
-    local recname=$1
+    local basedir=$1
+    local recname=$2
     # recipes extend the ENV script
-    source "${THIS_DIR}/ENV.sh" || return $?
+    source "${basedir}/ENV.sh" || return $?
     # variables for recipes
     export ENV_WORKING_DIR="${PWD}"
     export ENV_DEPLOY_DIR="${ENV_WORKING_DIR}/deploy"
-    export REC_UTIL_DIR="${THIS_DIR}/${recname}/utils"
+    export REC_UTIL_DIR="${basedir}/${recname}/utils"
     export REC_SRC_DIR="${ENV_WORKING_DIR}/src/${recname}"
     export REC_WORK_DIR="${ENV_WORKING_DIR}/work/${recname}"
     export REC_ENV_DIR="${ENV_WORKING_DIR}/env" # shared b/w recipes, shhh... ;)
-    source "${THIS_DIR}/${recname}.sh"
+    source "${basedir}/${recname}.sh"
 }
 
 function build_recipe_export_env_with_deps()
 {
-    local recname=$1
+    local basedir=$1
+    local recname=$2
     # we really just want DEPENDS_ENVIRONMENT to begin with
-    build_recipe_export_env_base "$recname" || return $?
+    build_recipe_export_env_base "$basedir" "$recname" || return $?
     # cache DEPENDS_ENVIRONMENT since it will be overridden by dependencies
     IFS=':' read -ra DEP_ENV_CACHE <<< "$DEPENDS_ENVIRONMENT"
     for de in "${DEP_ENV_CACHE[@]}"; do
         echo "$recname: depends: $de"
-        build_recipe_export_env_base "$de" || return $?
+        build_recipe_export_env_base "$basedir" "$de" || return $?
     done
     # now that we have exported from dependencies, we need our own environment
-    build_recipe_export_env_base "$recname"
+    build_recipe_export_env_base "$basedir" "$recname"
 }
 
-build_recipe_export_env_with_deps "$RECIPE"
+function build_recipe_env()
+{
+    local THIS_DIR
+    THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+    # Script options
+    local RECIPE=""
+    local WORKING_DIR=""
+    while getopts "r:w:h?" o; do
+        case "$o" in
+            r)
+                RECIPE="${OPTARG}"
+                ;;
+            w)
+                WORKING_DIR="${OPTARG}"
+                ;;
+            h)
+                usage
+                return
+                ;;
+            *)
+                echo "Unknown option"
+                usage
+                return 1
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
+    if [ -z "$RECIPE" ] || [ -z "$WORKING_DIR" ]; then
+        usage
+        return 1
+    fi
+    cd "$WORKING_DIR" || return $?
+    build_recipe_export_env_with_deps "$THIS_DIR" "$RECIPE"
+}
+
+OPTIND=1 # reset since we're probably being sourced
+build_recipe_env "$@"
