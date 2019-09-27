32ce6818dc39eadc62e88e1f665670a5e286672f
Connor Imes <cimes@isi.edu>
2019-09-27 11:28:40 -0400

    sdk/hpsc-eclipse: reformat repo and IU array declarations
    This makes it easier to comment out entries when they break...

--- build-recipes/sdk/hpsc-eclipse.sh
+++ build-recipes/sdk/hpsc-eclipse.sh
@@ -8,27 +8,30 @@ export WGET_OUTPUT_MD5="6087e4def4382fd334de658f9bde190b"
 export DO_BUILD_OUT_OF_SOURCE=1
 
 # Eclipse update sites
-ECLIPSE_REPOSITORIES=("http://download.eclipse.org/releases/photon"
-                      "http://download.eclipse.org/tm/updates/4.0/"
-                      "http://gnu-mcu-eclipse.netlify.com/v4-neon-updates/"
-                      "http://downloads.yoctoproject.org/releases/eclipse-plugin/2.6.1/oxygen"
-                      "http://download.eclipse.org/linuxtools/update-7.1.0")
+ECLIPSE_REPOSITORIES=(
+    "http://download.eclipse.org/releases/photon"
+    "http://download.eclipse.org/tm/updates/4.0/"
+    "http://gnu-mcu-eclipse.netlify.com/v4-neon-updates/"
+    "http://downloads.yoctoproject.org/releases/eclipse-plugin/2.6.1/oxygen"
+    "http://download.eclipse.org/linuxtools/update-7.1.0"
+)
 
 # Plugins to install
-ECLIPSE_PLUGIN_IUS=(org.yocto.doc.feature.group/1.4.1.201901082310
-                    org.yocto.sdk.feature.group/1.4.1.201901082311
-                    ilg.gnumcueclipse.core/4.5.1.201901011632
-                    ilg.gnumcueclipse.managedbuild.cross.arm/2.6.4.201901011632
-                    ilg.gnumcueclipse.debug.core/1.2.2.201901011632
-                    ilg.gnumcueclipse.templates.cortexm.feature.feature.group/1.4.4.201901011632
-                    org.eclipse.linuxtools.perf.feature.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.perf.remote.feature.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.perf.feature.source.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.perf.remote.feature.source.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.profiling.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.profiling.remote.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.profiling.source.feature.group/7.1.0.201812121718
-                    org.eclipse.linuxtools.profiling.remote.source.feature.group/7.1.0.201812121718
+ECLIPSE_PLUGIN_IUS=(
+    org.yocto.doc.feature.group/1.4.1.201901082310
+    org.yocto.sdk.feature.group/1.4.1.201901082311
+    ilg.gnumcueclipse.core/4.5.1.201901011632
+    ilg.gnumcueclipse.managedbuild.cross.arm/2.6.4.201901011632
+    ilg.gnumcueclipse.debug.core/1.2.2.201901011632
+    ilg.gnumcueclipse.templates.cortexm.feature.feature.group/1.4.4.201901011632
+    org.eclipse.linuxtools.perf.feature.feature.group/7.1.0.201812121718
+    org.eclipse.linuxtools.perf.remote.feature.feature.group/7.1.0.201812121718
+    org.eclipse.linuxtools.perf.feature.source.feature.group/7.1.0.201812121718
+    org.eclipse.linuxtools.perf.remote.feature.source.feature.group/7.1.0.201812121718
+    org.eclipse.linuxtools.profiling.feature.group/7.1.0.201812121718
+    org.eclipse.linuxtools.profiling.remote.feature.group/7.1.0.201812121718
+    org.eclipse.linuxtools.profiling.source.feature.group/7.1.0.201812121718
+    org.eclipse.linuxtools.profiling.remote.source.feature.group/7.1.0.201812121718
 )
 
 ECLIPSE_DIR=eclipse
