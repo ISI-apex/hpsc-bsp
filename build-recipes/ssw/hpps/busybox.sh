80eec82ded1edccb8e358d4ccfbe3e08d24dd9f2
Connor Imes <cimes@isi.edu>
2019-10-04 15:40:12 -0400

    ssw/hpps/busybox: rm GIT_BRANCH value quotes
    This is just for compatibility with upgrade scripts.
    There is no revision change.

--- build-recipes/ssw/hpps/busybox.sh
+++ build-recipes/ssw/hpps/busybox.sh
@@ -2,7 +2,7 @@
 
 export GIT_REPO="https://github.com/ISI-apex/busybox.git"
 export GIT_REV=39c03aff68b726458265df7b32376dfa6267927a
-export GIT_BRANCH="hpsc"
+export GIT_BRANCH=hpsc
 
 export DO_FETCH_ONLY=1
 
