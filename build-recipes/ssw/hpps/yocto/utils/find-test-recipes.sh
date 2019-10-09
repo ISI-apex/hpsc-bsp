3eeda9d0071e357193238c0989d674c1993ecd71
Connor Imes <cimes@isi.edu>
2019-10-09 08:54:23 -0400

    ssw/hpps/yocto: find-test-recipes: rm 'set -e'; check failures in-place

--- build-recipes/ssw/hpps/yocto/utils/find-test-recipes.sh
+++ build-recipes/ssw/hpps/yocto/utils/find-test-recipes.sh
@@ -3,7 +3,6 @@
 # Requires the bitbake environment to be active and working directory to be
 # the poky build directory (where sourcing oe-init-build-env leaves us).
 #
-set -e
 
 # Parse test module JSON file to get its recipe dependencies
 function get_json_dependent_recipes()
@@ -31,29 +30,39 @@ function find_layer_test_recipes_for_suites()
     # grep expression replaces "suite1 suite2 ..." with "suite1|suite2|..." so
     # JSONS only matches specified test modules
     # NOTE: assumes no newline characters or leading/trailing whitespace
-    local suite_e=$(echo "$*" | tr "[:blank:]" "|")
+    local suite_e
+    suite_e=$(echo "$*" | tr "[:blank:]" "|")
     if [ -z "$(echo "$suite_e" | tr -d "|")" ]; then
         # no test modules (or specified as empty string or whitespace) causes
         # function to match on all JSON files when we actually want to filter
-        return 
+        return
     fi
-    local JSONS=($(find_layer_test_mod_jsons "$laydir" | grep -E "$suite_e"))
+    local JSONS
+    JSONS=$(find_layer_test_mod_jsons "$laydir") || return $?
+    JSONS=($(echo "$JSONS" | grep -E "$suite_e"))
     local RECIPES=()
     for f in "${JSONS[@]}"; do
-        RECIPES+=($(get_json_dependent_recipes "$f"))
+        RECIPES+=($(get_json_dependent_recipes "$f")) || return $?
     done
     echo "${RECIPES[@]}" # note: may contain duplicate entries
 }
 
 function yocto_get_test_recipes()
 {
-    local TEST_SUITES=($(grep TEST_SUITES conf/local.conf | cut -d'=' -f2- | tr -d '"' | xargs))
-    local LAYERS=($(bitbake-layers show-layers | tail -n +4 | awk '{print $2}'))
+    # Parsing needs to be broken up like this to correctly detect errors
+    local TEST_SUITES
+    TEST_SUITES=$(grep TEST_SUITES conf/local.conf) || return $?
+    TEST_SUITES=($(echo "$TEST_SUITES" | cut -d'=' -f2- | tr -d '"' | xargs))
+    local LAYERS
+    LAYERS=$(bitbake-layers show-layers) || return $?
+    LAYERS=($(echo "$LAYERS" | tail -n +4 | awk '{print $2}'))
     local TRS=()
     for l in "${LAYERS[@]}"; do
-        TRS+=($(find_layer_test_recipes_for_suites "$l" "${TEST_SUITES[@]}"))
+        TRS+=($(find_layer_test_recipes_for_suites "$l" "${TEST_SUITES[@]}")) || \
+            return $?
     done
     # filter duplicate entries
+    local TEST_RECIPES
     TEST_RECIPES=($(echo "${TRS[@]}" | tr ' ' '\n' | sort -u))
     echo "${TEST_RECIPES[@]}"
 }
