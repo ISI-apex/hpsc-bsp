056f8153207c47ee08821c8de0fa43924a2de7ae
Kaushik Datta <kdatta@isi.edu>
2020-02-04 13:41:59 -0500

    ssw/hpps/yocto/meta-hpsc.sh: upgrade to rev: d1f86e7

--- build-recipes/ssw/hpps/yocto/meta-hpsc.sh
+++ build-recipes/ssw/hpps/yocto/meta-hpsc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_REPO="https://github.com/ISI-apex/meta-hpsc.git"
-export GIT_REV=23c4353c88a90713fa370d55d8f2c0d2e2e4a9b7
+export GIT_REV=d1f86e746969481c13a2002801d6f02a068de5f8
 export GIT_BRANCH=hpsc
 
 # Yocto layers aren't built
