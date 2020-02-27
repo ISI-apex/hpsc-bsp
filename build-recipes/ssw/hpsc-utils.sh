53d6a14e0b2c25fcbe0d4054dc7eb8f43961b5c0
Kaushik Datta <kdatta@isi.edu>
2020-02-27 11:52:03 -0500

    ssw/hpsc-utils.sh: upgrade to rev: 0393b4a

--- build-recipes/ssw/hpsc-utils.sh
+++ build-recipes/ssw/hpsc-utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_REPO="https://github.com/ISI-apex/hpsc-utils.git"
-export GIT_REV=e5b9f458526ca9d73342253c92b1f3b1147a7450
+export GIT_REV=0393b4a8f9110b4d5ce037c64acd46889890de21
 export GIT_BRANCH=hpsc
 
 DEPLOY_DIR_1=ssw/tests
@@ -11,8 +11,11 @@ DEPLOY_ARTIFACTS_1=(
     test/pytest/conftest.py
     test/pytest/measure_load.py
     test/pytest/test_dma.py
+    test/pytest/test_hotplug.py
     test/pytest/test_interrupt_affinity.py
     test/pytest/test_mbox.py
+    test/pytest/test_mbox_multi_system.py
+    test/pytest/test_mmu.py
     test/pytest/test_nand.py
     test/pytest/test_parallel_scaling.py
     test/pytest/test_shm.py
