a5dd4cc07fa3ceb8cf38e983c93a92b4abd62b9d
Connor Imes <cimes@isi.edu>
2020-03-20 20:21:35 -0400

    ssw/trch/rtems: rm unnecessary exes_to_uboot_fmt function

--- build-recipes/ssw/trch/rtems.sh
+++ build-recipes/ssw/trch/rtems.sh
@@ -19,22 +19,6 @@ function do_toolchain_uninstall()
     rm -rf "$RTEMS_TRCH_BSP"
 }
 
-function exes_to_uboot_fmt()
-{
-    while IFS= read -r -d '' file; do
-        # local BASE="$(echo "$file" | sed 's/.exe$//')"
-        local BASE="${file%.exe}" # strip .exe extension
-        echo "${BASE}: arm-rtems5-objcopy"
-        arm-rtems5-objcopy -O binary "${BASE}.exe" "${BASE}.bin" || return $?
-        echo "${BASE}: gzip"
-        gzip -f -9 "${BASE}.bin" || return $?
-        echo "${BASE}: mkimage"
-        mkimage -A arm -O rtems -T kernel -C gzip -a 70000000 -e 70000040 \
-                -n "$(basename "$file")" -d "${BASE}.bin.gz" "${BASE}.img" \
-                || return $?
-    done < <(find . -name "*.exe" -print0)
-}
-
 function do_build()
 {
     ENV_check_rsb_toolchain || return $?
@@ -57,8 +41,6 @@ function do_build()
                  # --enable-tests || return $?
     echo "rtems: hpsc_trch_qemu: make"
     make_parallel || return $?
-
-    exes_to_uboot_fmt || return $?
 }
 
 function do_toolchain_install()
