5095e373aa491e87890d586f184e3f9f3879a726
Alexei Colin <acolin@isi.edu>
2020-03-22 11:55:27 -0400

    ssw/hpps/yocto/meta-hpsc: update hpsc-utils

--- build-recipes/ssw/hpps/yocto/meta-hpsc.sh
+++ build-recipes/ssw/hpps/yocto/meta-hpsc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GIT_REPO="https://github.com/ISI-apex/meta-hpsc.git"
-export GIT_REV=30e0c1b1fdf849dc7f84a86caf749cdecac88095
+export GIT_REV=09508576c1d89bf00497bce22e414f36a197337e
 export GIT_BRANCH=hpsc
 
 # Yocto layers aren't built
